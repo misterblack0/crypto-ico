adc98e48b64c4da7284975e55551d07939e700d8
misterblack0 <misterblack0@protonmail.com>
2020-08-25 19:51:44 +0300

    faqs section

--- js/index.js
+++ js/index.js
@@ -47,3 +47,54 @@ window.onload = function () {
   let ctx = document.getElementById("chart-area").getContext("2d");
   window.myDoughnut = new Chart(ctx, config);
 };
+
+/* FAQS tabs for accordion */
+
+function openTab(event, tab) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tab).style.display = "block";
+  event.currentTarget.className += " active";
+}
+
+const defaultTab = document.getElementById("select-general");
+
+defaultTab.addEventListener("click", () => openTab(event, "tab-general"));
+
+document
+  .getElementById("select-ico")
+  .addEventListener("click", () => openTab(event, "tab-ico"));
+
+document
+  .getElementById("select-token")
+  .addEventListener("click", () => openTab(event, "tab-token"));
+
+document
+  .getElementById("select-investors")
+  .addEventListener("click", () => openTab(event, "tab-investors"));
+
+defaultTab.click(); // Show a tab by default
+
+/* FAQS Accordion */
+
+const acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
