481e0d3555e033a628d6843545de9a9db1a1c51e
misterblack0 <misterblack0@protonmail.com>
2020-09-18 18:07:52 +0300

    media q

--- js/index.js
+++ js/index.js
@@ -91,11 +91,10 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+
+    panel.style.maxHeight
+      ? (panel.style.maxHeight = null)
+      : (anel.style.maxHeight = panel.scrollHeight + "px");
   });
 }
 
